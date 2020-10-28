49743fe3f20fb41857b0d60482ef0180899d89aa
DoctorFook <a859910133@126.com>
2020-10-28 19:53:25 +0800

    router重定向和elementui导入

---
+++ src/views/Login/index.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="login">
+    登录页面123
+     <el-row>
+          <el-button type="success">主要按钮</el-button><el-button type="success">成功按钮</el-button>
+     </el-row>
+</div>
+</template>
+
+<script>
+export default {
+  name:'',
+  data(){
+    return {
+    }
+  },
+  props:{},
+  components:{},
+  watch:{},
+  computed:{},
+  methods:{},
+  created(){},
+  mounted(){}
+}
+</script>
+<style lang="scss" scoped>
+.login{
+    background-color: pink;
+    height: 100vh;
+}
+</style>
